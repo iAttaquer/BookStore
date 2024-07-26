6656c997e576c361e5686502be5af432857f7511
iAttaquer <iAttaquer@proton.me>
2024-07-26 14:16:35 +0200

    core

---
+++ src/DotNetBoilerplate.Infrastructure/DAL/Repositories/InMemoryCatalogRepository.cs
@@ -0,0 +1,14 @@
+﻿using DotNetBoilerplate.Core.Catalogs;
+
+namespace DotNetBoilerplate.Infrastructure.DAL.Repositories;
+
+internal sealed class InMemoryCatalogRepository : ICatalogRepository
+{
+    private readonly List<Catalog> _catalogs = [];
+
+    public Task AddAsync(Catalog catalog)
+    {
+        _catalogs.Add(catalog);
+        return Task.CompletedTask;
+    }
+}
