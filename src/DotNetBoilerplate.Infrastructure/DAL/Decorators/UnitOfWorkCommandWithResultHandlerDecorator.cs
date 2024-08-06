d6f24e08fa65c6e10806034f82c52563de595d80
Albert Szymański <alb.szym1@gmail.com>
2024-08-06 14:44:18 +0200

    feat: added UoW decorator for commands with results

---
+++ src/DotNetBoilerplate.Infrastructure/DAL/Decorators/UnitOfWorkCommandWithResultHandlerDecorator.cs
@@ -0,0 +1,17 @@
+﻿using DotNetBoilerplate.Shared.Abstractions.Commands;
+
+namespace DotNetBoilerplate.Infrastructure.DAL.Decorators;
+
+internal sealed class
+    UnitOfWorkCommandWithResultHandlerDecorator<TCommand, TResult>(
+        ICommandHandler<TCommand, TResult> commandHandler,
+        IUnitOfWork unitOfWork)
+    : ICommandHandler<TCommand, TResult>
+    where TCommand : class, ICommand
+{
+    public async Task<TResult> HandleAsync(TCommand command)
+    {
+        var result = await unitOfWork.ExecuteAsync(() => commandHandler.HandleAsync(command));
+        return result;
+    }
+}
